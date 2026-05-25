5e64a38496a5bd1a04d41297e8a36ace3a5a5fd1
Shibangrg <grgshiv15@gmail.com>
2026-05-25 21:19:14 +0530

    dash
    dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Added for *ngIf
+import { Component, inject } from '@angular/core'; // Added inject
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service'; // Added AuthService
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Added components
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Inject the Auth Service
+  public authService = inject(AuthService); 
 
   title = 'digital-signature-app';
-
   menuOpen = false;
 
   toggleMenu(): void {
@@ -21,5 +24,4 @@ export class AppComponent {
   closeMenu(): void {
     this.menuOpen = false;
   }
-
 }
